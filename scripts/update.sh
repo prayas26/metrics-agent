f7128c6bc6ed0a7db3eddcf6dcf68c63923d6f98
Brett <blockloop@users.noreply.github.com>
2018-10-16 15:17:15 -0500

    show terse output on updates (#52)

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@
 main() {
 	if command -v apt-get 2&>/dev/null; then
 		apt-get update -qq
-		apt-get install -qq -y --only-upgrade metrics-agent
+		apt-get install -q -y --only-upgrade metrics-agent
 	elif command -v yum 2&>/dev/null; then
 		yum -q -y update metrics-agent
 	fi
